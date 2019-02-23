0327f0ac07ab4e6375e6dc38860ccc8037a26621
river0 <victor@river0.uk>
2019-02-23 17:15:22 +0000

    very basic react setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+import './styles/app.scss'
+
+class App extends Component {
+  render() {
+    return (
+    <h1>Victor Rivero</h1>
+    );
+  }
+}
+
+export default App;
